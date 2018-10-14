ca1611d899db0d0cd431041e9536f15fa8aa524f
Aaron Saray <me@aaronsaray.com>
2018-10-13 21:25:07 -0500

    obviously not working async

--- src/index.js
+++ src/index.js
@@ -8,35 +8,55 @@ const evilscan = require("evilscan");
 const mysql = require("mysql");
 const Table = require("cli-table");
 
-/**
- * Now run it
- */
-let results = {
-  T: [],
-  R: [],
-  O: [],
-  U: []
-};
+const STATUS_TIMEOUT = "Time out";
+const STATUS_REFUSED = "Refused";
+const STATUS_OPEN = "Open";
+const STATUS_UNREACHABLE = "Unreachable";
+const STATUS_AUTHENTICATED = "Authenticated";
+
+const MYSQL_PORT = "3306";
 
 let options = {
   target: "127.0.0.1",
-  port: "3306",
-  status: "TROU", // Timeout, Refused, Open, Unreachable
-  banner: true
+  port: MYSQL_PORT,
+  status: "TROU"
 };
+
+let results = [];
 
 let scanner = new evilscan(options);
 
 scanner.on("result", function(data) {
+  let type;
+
   if (data.status.match(/timeout/i)) {
-    results.T.push(data.ip);
+    type = STATUS_TIMEOUT;
   } else if (data.status.match(/refuse/i)) {
-    results.R.push(data.ip);
+    type = STATUS_REFUSED;
   } else if (data.status.match(/unreachable/i)) {
-    results.U.push(data.ip);
+    type = STATUS_UNREACHABLE;
   } else {
-    results.O.push(data.ip);
+    type = STATUS_OPEN;
+
+    let connection = mysql.createConnection({
+      host: data.ip,
+      port: MYSQL_PORT,
+      user: "root",
+      connectTimeout: 1500
+    });
+
+    connection.connect(err => {
+      if (!err) {
+        type = STATUS_AUTHENTICATED;
+      }
+      connection.destroy();
+    });
   }
+
+  results.push({
+    type,
+    ip: data.ip
+  });
 });
 
 scanner.on("error", function(err) {
@@ -44,14 +64,13 @@ scanner.on("error", function(err) {
 });
 
 scanner.on("done", function() {
-  console.log("The following IPs had port 3306 open.");
-  console.log(results.O);
   let table = new Table({
-    head: ["item"]
+    head: ["IP", "Port", "Status"]
   });
-  table.push(results.O);
+  results.forEach(result => {
+    table.push([result.ip, MYSQL_PORT, result.type]);
+  });
   console.log(table.toString());
-  // this is where we'd pass this on to a connection to mysql try
 });
 
 scanner.run();
