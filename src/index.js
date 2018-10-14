94e09aa0fba388fbc294dc131e1ae3b6798a99be
Aaron Saray <me@aaronsaray.com>
2018-10-13 21:47:52 -0500

    changed finally to async

--- src/index.js
+++ src/index.js
@@ -16,61 +16,95 @@ const STATUS_AUTHENTICATED = "Authenticated";
 
 const MYSQL_PORT = "3306";
 
-let options = {
-  target: "127.0.0.1",
-  port: MYSQL_PORT,
-  status: "TROU"
-};
+/**
+ * Runs the scan
+ *
+ * Returns a promise of the scan results
+ * that will have all the results by type in an array
+ */
+function portScan() {
+  let results = [];
 
-let results = [];
-
-let scanner = new evilscan(options);
-
-scanner.on("result", function(data) {
-  let type;
-
-  if (data.status.match(/timeout/i)) {
-    type = STATUS_TIMEOUT;
-  } else if (data.status.match(/refuse/i)) {
-    type = STATUS_REFUSED;
-  } else if (data.status.match(/unreachable/i)) {
-    type = STATUS_UNREACHABLE;
-  } else {
-    type = STATUS_OPEN;
-
-    let connection = mysql.createConnection({
-      host: data.ip,
+  return new Promise((resolve, reject) => {
+    let options = {
+      target: "127.0.0.1",
       port: MYSQL_PORT,
-      user: "root",
-      connectTimeout: 1500
+      status: "TROU"
+    };
+
+    let scanner = new evilscan(options);
+
+    scanner.on("error", err => {
+      reject(err);
     });
 
-    connection.connect(err => {
-      if (!err) {
-        type = STATUS_AUTHENTICATED;
+    scanner.on("done", () => {
+      resolve(results);
+    });
+
+    scanner.on("result", data => {
+      let type;
+
+      if (data.status.match(/timeout/i)) {
+        type = STATUS_TIMEOUT;
+      } else if (data.status.match(/refuse/i)) {
+        type = STATUS_REFUSED;
+      } else if (data.status.match(/unreachable/i)) {
+        type = STATUS_UNREACHABLE;
+      } else {
+        type = STATUS_OPEN;
       }
-      connection.destroy();
+
+      results.push({
+        type,
+        ip: data.ip
+      });
     });
-  }
 
-  results.push({
-    type,
-    ip: data.ip
+    scanner.run();
   });
-});
+}
 
-scanner.on("error", function(err) {
-  throw new Error(data.toString());
-});
+function checkConnectMysql(result) {
+  return new Promise((resolve, reject) => {
+    if (result.type == STATUS_OPEN) {
+      let connection = mysql.createConnection({
+        host: result.ip,
+        port: MYSQL_PORT,
+        user: "root",
+        connectTimeout: 1500
+      });
 
-scanner.on("done", function() {
-  let table = new Table({
-    head: ["IP", "Port", "Status"]
+      connection.connect(err => {
+        if (err) {
+          reject(err);
+        }
+        connection.destroy();
+        result.type = STATUS_AUTHENTICATED;
+        resolve(result);
+      });
+    } else {
+      resolve(result);
+    }
   });
-  results.forEach(result => {
-    table.push([result.ip, MYSQL_PORT, result.type]);
-  });
-  console.log(table.toString());
-});
+}
 
-scanner.run();
+portScan()
+  .then(results => {
+    let promises = [];
+
+    results.forEach(result => {
+      promises.push(checkConnectMysql(result));
+    });
+
+    return Promise.all(promises);
+  })
+  .then(items => {
+    let table = new Table({
+      head: ["IP", "Port", "Status"]
+    });
+    items.forEach(result => {
+      table.push([result.ip, MYSQL_PORT, result.type]);
+    });
+    console.log(table.toString());
+  });
